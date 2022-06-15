a21102e43edd4dc88639d2daa2a23cd214e271cd
jhamil545 <jhamil.apaza@upeu.edu.pe>
2022-06-14 19:42:22 -0500

    examen 2

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/main', function(req, res, next) {
+  res.render('main');
+});
+
+router.get('/add-rnuevocolegiado', function(req, res, next) {
+  res.render('rnuevocolegiado/add');
+});
+
+router.get('/add-tramitedocumentario', function(req, res, next) {
+  res.render('tramitedocumentario/add');
+});
+
+
+module.exports = router;
+
